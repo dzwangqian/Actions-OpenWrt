698fdae609d6b6706496fec22173ac1854b41526
dzwangqian <113566938+dzwangqian@users.noreply.github.com>
2022-09-22 15:44:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.250/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
